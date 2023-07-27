796e4577db809ea935729675726679e494486262
abbassDaher <abbassDaher1@outlook.com>
2023-07-27 16:10:10 +0300

    The code  is defining a method called  on the prototype of the  object. This means that all instances of the  object will have access to this method.

--- js/main.js
+++ js/main.js
@@ -14,21 +14,35 @@ saskiiHealth.style.width = "100%"
 nartooHealth.style.width = "100%"
 
 
+/**
+ * The function creates a character object with properties for name, strength, and health.
+ * @param name - The name of the character. It can be any string value.
+ * @param strength - The strength parameter represents the character's physical power or ability to
+ * inflict damage on opponents. It can be measured in terms of attack power or damage output.
+ * @param health - The "health" parameter represents the amount of health or hit points that a
+ * character has. It determines how much damage the character can take before being defeated.
+ */
 function chracter(name,strength,health){
     this.name = name
     this.strength = strength
     this.health = health
 }
 
+/* The code `chracter.prototype.attack` is defining a method called `attack` on the prototype of the
+`chracter` object. This means that all instances of the `chracter` object will have access to this
+method. */
+/* The code `chracter.prototype.attack` is defining a method called `attack` on the prototype of the
+`chracter` object. This means that all instances of the `chracter` object will have access to this
+method. */
 chracter.prototype.attack = function(opponent){
     console.log("this",this);
     console.log("opponent",opponent)
     if(this.name == 'naroto' ){
-        opponent.health -=10
+        opponent.health -=this.strength
         saskiiHealth.style.width = `${opponent.health}%`
         console.log(`${opponent.health}%`);
     }else{
-        opponent.health -=5
+        opponent.health -=this.strength
         console.log(`${opponent.health}%`);
         nartooHealth.style.width = `${opponent.health}%`
 
