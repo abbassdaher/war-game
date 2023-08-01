5250a16980c5d914ef445a337b1ff4c6c27a24d9
abbassDaher <abbassDaher1@outlook.com>
2023-08-01 13:06:04 +0300

    function is a method that increases the health of a character

--- js/main.js
+++ js/main.js
@@ -1,17 +1,14 @@
-let saskiiAttackBTN = document.querySelector(".saskii-attack-btn")
-let narotoAttackBTN = document.querySelector(".narto-attack-btn")
+// let saskiiAttackBTN = document.querySelector(".saskii-attack-btn")
+// let narotoAttackBTN = document.querySelector(".narto-attack-btn")
 
-let saskiiHealthBTN = document.querySelector(".saskii-health-btn")
-let narotoHealthBTN = document.querySelector(".narto-health-btn")  
+// let saskiiHealthBTN = document.querySelector(".saskii-health-btn")
+// let narotoHealthBTN = document.querySelector(".narto-health-btn")  
 
-let nartooHealth = document.querySelector(".nartoo-health")
-let saskiiHealth = document.querySelector(".saskii-health")
+// let nartooHealth = document.querySelector(".nartoo-health")
+// let saskiiHealth = document.querySelector(".saskii-health")
 
- let saskiHeathyWidth 
- let narotoHealthWidth
-
-saskiiHealth.style.width = "100%"
-nartooHealth.style.width = "100%"
+// saskiiHealth.style.width = "100%"
+// nartooHealth.style.width = "100%"
 
 
 /**
@@ -22,40 +19,53 @@ nartooHealth.style.width = "100%"
  * @param health - The "health" parameter represents the amount of health or hit points that a
  * character has. It determines how much damage the character can take before being defeated.
  */
-function chracter(name,strength,health){
+function chracter(name, strength, health) {
     this.name = name
     this.strength = strength
     this.health = health
+
+    this.attackBTN = document.querySelector(`.${this.name}-attack-btn`)
+    this.healthBTN = document.querySelector(`.${this.name}-health-btn`)
+    this.progress = document.querySelector(`.${this.name}-health`)
+
+
 }
 
 /* The code `chracter.prototype.attack` is defining a method called `attack` on the prototype of the
 `chracter` object. This means that all instances of the `chracter` object will have access to this
 method. */
-/* The code `chracter.prototype.attack` is defining a method called `attack` on the prototype of the
-`chracter` object. This means that all instances of the `chracter` object will have access to this
-method. */
-chracter.prototype.attack = function(opponent){
-    console.log("this",this);
-    console.log("opponent",opponent)
-    if(this.name == 'naroto' ){
-        opponent.health -=this.strength
-        saskiiHealth.style.width = `${opponent.health}%`
-        console.log(`${opponent.health}%`);
-    }else{
-        opponent.health -=this.strength
-        console.log(`${opponent.health}%`);
-        nartooHealth.style.width = `${opponent.health}%`
+chracter.prototype.attack = function (opponent) {
+    opponent.health -= this.strength
+    opponent.progress.style.width = `${opponent.health}%`
+    console.log(`${opponent.health}%`);
+}
 
+
+
+chracter.prototype.makeHealthy = function () {
+    if (this.health < 100) {
+        this.health += this.strength
+        if (this.health > 100)
+            this.health = 100
+        this.progress.style.width = `${this.health}%`
+        console.log(`${this.health}%`);
     }
-    
+
+}
+CharacterData.prototype.status = function () {
+    console.log("Name: " + this.name);
+    console.log("Strength: " + this.strength);
+    console.log("health: " + this.health);
+    console.log("attackBTN: " + this.attackBTN)
 }
 
-let naroto = new chracter('naroto',10,100)
-let sasaki = new chracter('sasaki',5,100)
+let nartoo = new chracter('nartoo', 10, 100)
+let saskii = new chracter('saskii', 5, 100)
 
+// saskii.status()
+console.log(nartoo)
+nartoo.attackBTN.addEventListener('click', function () { nartoo.attack(saskii); })
+saskii.attackBTN.addEventListener('click', function () { saskii.attack(nartoo); })
 
-narotoAttackBTN.addEventListener('click',function(){naroto.attack(sasaki)})
-saskiiAttackBTN.addEventListener('click',function(){sasaki.attack(naroto)})
-
-console.log(naroto)
-console.log(sasaki)
+saskii.healthBTN.addEventListener('click', function () { saskii.makeHealthy() })
+nartoo.healthBTN.addEventListener('click', function () { nartoo.makeHealthy() })
