cccc3fbabda9e4c66a3e46a6362241bc5588a81f
abbassDaher <abbassDaher1@outlook.com>
2023-08-02 09:19:45 +0300

    build some characterstics This is used to display the game over message for the opponent character

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-// let saskiiAttackBTN = document.querySelector(".saskii-attack-btn")
-// let narotoAttackBTN = document.querySelector(".narto-attack-btn")
-
-// let saskiiHealthBTN = document.querySelector(".saskii-health-btn")
-// let narotoHealthBTN = document.querySelector(".narto-health-btn")  
-
-// let nartooHealth = document.querySelector(".nartoo-health")
-// let saskiiHealth = document.querySelector(".saskii-health")
-
-// saskiiHealth.style.width = "100%"
-// nartooHealth.style.width = "100%"
-
-
 /**
  * The function creates a character object with properties for name, strength, and health.
  * @param name - The name of the character. It can be any string value.
@@ -27,6 +14,8 @@ function chracter(name, strength, health) {
     this.attackBTN = document.querySelector(`.${this.name}-attack-btn`)
     this.healthBTN = document.querySelector(`.${this.name}-health-btn`)
     this.progress = document.querySelector(`.${this.name}-health`)
+    this.controller = document.querySelector(`.${this.name}-controller`)
+    this.result = document.querySelector(`.${this.name}-result`)
 
 
 }
@@ -35,15 +24,30 @@ function chracter(name, strength, health) {
 `chracter` object. This means that all instances of the `chracter` object will have access to this
 method. */
 chracter.prototype.attack = function (opponent) {
-    opponent.health -= this.strength
-    opponent.progress.style.width = `${opponent.health}%`
-    console.log(`${opponent.health}%`);
+    if (opponent.health == 0 || this.health == 0) {
+        opponent.controller.style.visibility = "hidden";
+        /* The code `opponent.result.innerHTML  ="Game Over"` sets the innerHTML of the `result`
+        element of the opponent character to "Game Over". This is used to display the game over
+        message for the opponent character. */
+        opponent.result.innerHTML  ="Game Over"
+        this.result.innerHTML = "Congratulation"
+        this.result.style.color = "green"
+        opponent.result.style.color = "red"
+        this.controller.style.visibility = "hidden";
+
+    } else {
+        opponent.health -= this.strength
+        opponent.progress.style.width = `${opponent.health}%`
+        console.log(`${opponent.health}%`);
+    }
+
 }
 
 
 
+/* The `chracter.prototype.makeHealthy` function is a method that increases the health of a character. */
 chracter.prototype.makeHealthy = function () {
-    if (this.health < 100) {
+    if (this.health < 100 && this.health > 0) {
         this.health += this.strength
         if (this.health > 100)
             this.health = 100
